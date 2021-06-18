0aa98fcd38a71bbd24976132eef4d23ad7e3cf51
Parth Nonghanvadra <parth.nonghanvadra@gatewaygroup.com>
2021-06-18 16:08:17 +0530

    C# Advance Training Assignment 3 Added

---
+++ C#_Advance_Assignment/CSharp_Recap/CSharp_Recap/Assignment_3/Assignment_3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Recap.Assignment_3
+{
+    public class Assignment_3
+    {
+        public static void Execute()
+        {
+            var city1 = new City() { Id = 1, Name = "City1", Population = 60 };
+            var city2 = new City() { Id = 2, Name = "City2", Population = 250 };
+            var city3 = new City() { Id = 3, Name = "City3", Population = 0 };
+            var city4 = new City() { Id = 4, Name = "City4" };
+
+            GetMyCityName(city1);
+            GetMyCityName(city2);
+            GetMyCityName(city3);
+            GetMyCityName(city4);
+
+            Console.WriteLine("\n==========================================");
+            Console.WriteLine(Environment.NewLine + " Process Completed " + Environment.NewLine);
+        }
+        static void GetMyCityName(ILocation location)
+        {
+            switch (location)
+            {
+                case City e when e.Population == 0:
+                    Console.WriteLine("Population Data is Invalid/Zero");
+                    break;
+                case City e when e.Population > 100:
+                    Console.WriteLine("Population not in control");
+                    break;
+                case City e when e.Population < 100:
+                    Console.WriteLine("Population under control");
+                    break;
+                case City e when e.Population is null:
+                    Console.WriteLine("Population Data is Invalid/Null");
+                    break;
+                default:
+                    Console.WriteLine("Invalid data");
+                    break;
+
+            }
+        }
+    }
+
+    public interface ILocation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Population { get; set; }
+    }
+
+    public class City : ILocation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Population { get; set; }
+    }
+}
--- C#_Advance_Assignment/CSharp_Recap/CSharp_Recap/Program.cs
+++ C#_Advance_Assignment/CSharp_Recap/CSharp_Recap/Program.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CSharp_Recap
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("================================ Assignment 1 ================================\n");
-            Console.WriteLine("=================================================================\r\n");
+            Console.WriteLine("================================ Assignment 1 ================================");
+            Console.WriteLine("==============================================================================\r\n");
             Assignment_1.Assignment_1.Execute();
 
-            Console.WriteLine("\r\n================================ Assignment 2 ================================\n");
-            _ = Assignment_2.Assignment_2.Execute();
+            Console.WriteLine("\r\n================================ Assignment 2 ================================");
+            Console.WriteLine("==============================================================================\r\n");
+            await Assignment_2.Assignment_2.Execute();
 
+            Console.WriteLine("\r\n================================ Assignment 3 ================================");
+            Console.WriteLine("==============================================================================\r\n");
+            Assignment_3.Assignment_3.Execute();
 
             Console.ReadLine();
 
